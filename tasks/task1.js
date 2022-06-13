88b1a9a38487108280b51b6032cd8d4a65c2ff24
Eduard Chyzhyk <e.chyzhyk@mageworx.com>
2022-06-13 21:37:34 +0200

    calc with memoize

--- tasks/task1.js
+++ tasks/task1.js
@@ -4,3 +4,27 @@
 Создать функцию, которая принимает произвольное количество целочисленных параметров, находит их сумму и мемоизирует результат.
  */
 
+function memoizeGetSum(defaultData) {
+    const results = {...defaultData};
+
+    return function getSum(...args) {
+        const argsStr = args.join("|");
+        if (!results[argsStr]) {
+            console.log("new calculation");
+            console.log(argsStr);
+            results[argsStr] = args.reduce((sum, newEl) => sum + newEl);
+        }
+
+        return results[argsStr];
+    }
+}
+
+const getSum = memoizeGetSum();
+getSum(10, 20, 10);
+getSum(10, 20, 10);
+getSum(10, 20, -10);
+getSum(10, 20, 10, 1);
+getSum(10, 20, 10, 3);
+getSum(10, 20, 10, 1);
+getSum(10, 20, -10);
+
