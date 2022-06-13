f22390dce6765b0298d5cfbae6e59e09be66b379
Eduard Chyzhyk <e.chyzhyk@mageworx.com>
2022-06-13 21:42:09 +0200

    calc memoize update

--- tasks/task1.js
+++ tasks/task1.js
@@ -20,6 +20,7 @@ function memoizeGetSum(defaultData) {
 }
 
 const getSum = memoizeGetSum();
+/*
 getSum(10, 20, 10);
 getSum(10, 20, 10);
 getSum(10, 20, -10);
@@ -27,4 +28,33 @@ getSum(10, 20, 10, 1);
 getSum(10, 20, 10, 3);
 getSum(10, 20, 10, 1);
 getSum(10, 20, -10);
+*/
 
+
+// с возможностью в любой момент времени заглянуть в кэш
+function memoizeGetSum2(defaultData) {
+    const results = {...defaultData};
+
+    function getSum(...args) {
+        const argsStr = args.join("|");
+        if (!getSum.results[argsStr]) {
+            console.log("new calculation");
+            console.log(argsStr);
+            getSum.results[argsStr] = args.reduce((sum, newEl) => sum + newEl);
+        }
+
+        return getSum.results[argsStr];
+    }
+
+    getSum.results = results;
+
+    return getSum;
+}
+
+const getSum2 = memoizeGetSum2();
+getSum2(-10, -20, 30, 50);
+getSum2(-10, -20, 30, 50);
+console.log(getSum2.results);
+getSum2(-10, -20, 30, 50, 3);
+getSum2(-10, -20, 30, 50, 4);
+console.log(getSum2.results);
